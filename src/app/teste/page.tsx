c5a53cefb374f73748fa89e28d8f33d3a816efe8
mabiiak <mabiane.polniak@gmail.com>
2023-05-20 13:02:18 -0300

    componentiza pagina - nlw aula 3

--- src/app/teste/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Teste() {
-  return (
-    <div>
-      <h1 className="bg-red-500 text-2xl font-bold uppercase">teste...</h1>
-      <p>louco</p>
-    </div>
-  )
-}
---
+++ src/components/Copyrigth.tsx
@@ -0,0 +1,16 @@
+export function Copyrigth() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        className="underline hover:text-gray-100"
+        target="_blank"
+        rel="noreferrer"
+        href="https://rocketseat.com.br/"
+      >
+        {' '}
+        Rocketseat
+      </a>
+    </div>
+  )
+}
